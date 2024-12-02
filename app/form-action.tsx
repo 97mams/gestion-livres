308c6205aff8b5bd3e5dd717d6caec719344f6f8
97mams <97mams@gmail.com>
2024-12-02 13:17:30 +0300

    add book success

--- app/form-action.tsx
+++ app/form-action.tsx
@@ -1,18 +1,25 @@
 "use client"
 import { Input } from "@/src/components/input"
 import { Button } from "@/src/components/button"
+import { createBookAction } from "./form.action"
 
 export function FormAction() {
 
-    const onSubmit = (formData: FormData) => {
-        console.log(formData);
+    const onSubmit = async (formData: FormData) => {
+        await createBookAction({
+            title: String(formData.get('title')),
+            author: String(formData.get('author')),
+            types: String(formData.get('types')),
+            date_publish: String(formData.get('date'))
+        })
     }
 
     return (
         <form action={onSubmit} className="flex flex-col gap-2">
             <Input type="text" name="title" id="title" children="mams" />
             <Input type="text" name="author" id="author" children="Auteur" />
-            <Input type="type" name="type" id="type" children="Genre" />
+            <Input type="type" name="types" id="type" children="Genre" />
+            <Input type="type" name="date" id="date-pub" children="Date de publication" />
             <Button type="submit" className="w-full">submit</Button>
         </form>
     )
--- app/form.action.ts
+++ app/form.action.ts
@@ -1,3 +1,26 @@
-export function createBookAction() {
+"use server"
 
+import { prisma } from "@/src/lib/prisma"
+import { redirect } from "next/navigation"
+
+export async function createBookAction(book: {
+    title: string,
+    author: string,
+    types: string,
+    date_publish: string
+}) {
+    const newBook = await prisma.books.create({
+        data: {
+            title: book.title,
+            author: book.author,
+            types: book.types,
+            date_publish: book.date_publish
+        }
+    })
+
+    if (newBook) {
+        redirect('/')
+    } else {
+        new Error("Reccoding fails")
+    }
 }
