7ebed88d594f18a9635ecfcfc514746fa2daf319
97mams <97mams@gmail.com>
2024-12-02 11:40:55 +0300

    create schema prisma

---
+++ prisma/migrations/20241202083955_bookmigration/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Books" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "types" TEXT NOT NULL,
+    "date_publish" TEXT NOT NULL
+);
