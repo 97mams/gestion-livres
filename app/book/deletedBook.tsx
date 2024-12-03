f6ef660aa67d5c3b149e96a56d19aa253d744ec0
97mams <97mams@gmail.com>
2024-12-03 09:51:55 +0300

    feat add buttn delete

---
+++ app/book/deletedBook.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { DeletedBookAction } from "../book.action";
+
+export function DeletedButton(params: { bookId: number }) {
+    const onDelete = async (id: number) => {
+        await DeletedBookAction(id)
+    }
+
+    return (
+        <button className="border border-red-400 text-red-400 hover:bg-red-200 text-sm rounded-md p-2" onClick={() => onDelete(params.bookId)}>
+            supr
+        </button>
+    )
+}
