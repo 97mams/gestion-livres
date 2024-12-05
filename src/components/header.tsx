4617dfc025ab54749419fcae63dc8750f8d24dc3
97mams <97mams@gmail.com>
2024-12-05 05:35:38 +0300

    fix img

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image"
+import { faker } from "@faker-js/faker"
+import Link from "next/link"
+
 export function Header() {
+
     return (
         <div className="w-full px-8 py-4 shadow-md flex items-baseline justify-between">
-            <h1 className="text-2xl text-zinc-900 line-through">M</h1>
+            <Link href="/" >
+                <img src={"/vercel.svg"} alt={"log"} className="w-8 h-8 rounded-full bg-gray-900 p-2" />
+            </Link>
             <div>
                 <a href="/" className="px-3 py-2 border border-zinc-500 text-zinc-500 hover:bg-zinc-100 rounded-md">s'inscrire</a>
             </div>
