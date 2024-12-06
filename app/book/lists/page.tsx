9b3fc3b9c97095531b4af69509125d2be1de5cea
97mams <97mams@gmail.com>
2024-12-06 20:53:50 +0300

    learn nextui :)

--- app/book/lists/page.tsx
+++ app/book/lists/page.tsx
@@ -1,61 +1,11 @@
-import { prisma } from "@/src/lib/prisma";
-import { Table, TableHeader, TableTh, TableBody, TableTd, TableTr } from "@/src/components/table";
-import Link from "next/link";
-import { DeletedButton } from "../deletedBook";
+import Card from "@mui/material/Card"
+import CardTitle from "@mui/material/Card"
+import Button from "@mui/material/Button"
 
-export default async function Page() {
-
-    const bookLists = await prisma.books.findMany()
-
+export default function Page() {
     return (
         <div>
-            <h1 className="text-black text-3xl font-bold">Liste livres</h1>
-            <Table>
-                <TableHeader>
-                    <TableTh>Auteur</TableTh>
-                    <TableTh>Titre</TableTh>
-                    <TableTh>Genre</TableTh>
-                    <TableTh>Date de sortie</TableTh>
-                    <TableTh>Action</TableTh>
-                </TableHeader>
-                <TableBody>
-                    {bookLists.map(book => (
-                        <TableTr key={book.id}>
-
-                            <TableTd>
-                                {book.author}
-                            </TableTd>
-                            <TableTd>
-                                {book.title}
-                            </TableTd>
-                            <TableTd>
-                                {book.types}
-                            </TableTd>
-                            <TableTd>
-                                {book.date_publish}
-                            </TableTd>
-                            <TableTd>
-                                <div className="flex gap-2">
-                                    <EditButton bookId={book.id} />
-                                    <DeletedButton bookId={book.id} />
-                                    <Link href={`/book/${book.id}`}>voir plus...</Link>
-                                </div>
-                            </TableTd>
-                        </TableTr>
-                    ))}
-                </TableBody>
-            </Table>
+            <Button color="secondary">Click</Button>
         </div>
-    )
-}
-
-const EditButton = (props: { bookId?: number }) => {
-    return (
-        <Link
-            className="border text-sm text-blue-700 border-blue-700 rounded-md p-2 hover:bg-blue-300"
-            href={"/"}
-        >
-            edit
-        </Link>
     )
 }
