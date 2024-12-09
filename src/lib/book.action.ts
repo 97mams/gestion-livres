437ba233cd3292043a4e6c477afb7bd9ac7de082
97mams <97mams@gmail.com>
2024-12-09 21:37:41 +0300

    fix directory

---
+++ src/lib/book.action.ts
@@ -0,0 +1,43 @@
+"use server"
+
+import { prisma } from "@/src/lib/prisma"
+import { redirect } from "next/navigation"
+
+export async function createBookAction(book: {
+    title: string,
+    author: string,
+    types: string,
+    resume: string,
+    mockupImages: string
+    date_publish: string
+}) {
+    const newBook = await prisma.books.create({
+        data: {
+            title: book.title,
+            author: book.author,
+            types: book.types,
+            resume: book.resume,
+            mockupImages: book.mockupImages,
+            date_publish: book.date_publish
+        }
+    })
+
+    if (newBook) {
+        redirect('/book/list')
+    } else {
+        new Error("Reccoding fails")
+    }
+}
+
+export async function DeletedBookAction(id: number) {
+    const deleted = await prisma.books.delete({
+        where: {
+            id: id
+        }
+    })
+
+    if (deleted) {
+        redirect('/book/lists')
+    }
+    return null
+}
