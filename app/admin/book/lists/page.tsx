b5fa4446a7d0a1af44abf565f61d12916f0643e6
97mams <97mams@gmail.com>
2024-12-12 19:46:20 +0300

    add feat card

---
+++ app/admin/book/lists/page.tsx
@@ -0,0 +1,31 @@
+import { Table, TableHeader, TableTd, TableTr, TableBody } from "@/src/components/table"
+import { prisma } from "@/src/lib/prisma"
+
+export default async function Page() {
+    const tableTitle = ["titre", "auteur", "description", "vote", "sortie"]
+
+    const bookResult = await prisma.books.findMany({
+        orderBy: [{ createdAt: "desc" }]
+    })
+
+    return (
+        <div className="w-full">
+            <Table>
+                <TableHeader>
+                    {tableTitle.map((title, key) => (<TableTd key={key}>{title}</TableTd>))}
+                </TableHeader>
+                <TableBody>
+                    {bookResult.map((book) => (
+                        <TableTr key={book.id}>
+                            <TableTd>{book.title}</TableTd>
+                            <TableTd>{book.author}</TableTd>
+                            <TableTd>{book.resume}</TableTd>
+                            <TableTd>{book.upvotes}</TableTd>
+                            <TableTd>{book.date_publish}</TableTd>
+                        </TableTr>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    )
+}
