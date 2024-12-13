2b81769943f2d97aa4a72af4df711c1034eaa3b6
97mams <97mams@gmail.com>
2024-12-13 18:50:59 +0300

    fix changed title button in h1

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 export async function SideBar(props: { items: String[] }) {
     return (
-        <div className="w-60 border-r px-4 border-r-gray-700 min-h-screen">
-            <button className="text-xl w-full mt-8 bg-gray-900 rounded-sm p-2">Genres</button>
+        <div className="w-60 border-r px-4 border-r-gray-700 min-h-screen rounded-md">
+            <p className="text-xl w-full mt-8 bg-gray-900 rounded-sm p-2">Genres</p>
             <ul className="w-full flex flex-col gap-2 mt-2 ">
                 {
                     props.items.map((item, key) => (
@@ -11,7 +11,7 @@ export async function SideBar(props: { items: String[] }) {
                             key={key}
                             className="w-full hover:line-through"
                         >
-                            <Link href={""}>{item}</Link>
+                            <Link href={`/book/lists/${item}`}>{item}</Link>
                         </li>
                     ))
                 }
