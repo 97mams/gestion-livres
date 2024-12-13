b5a5c1e98e55bbaffeeb62e25f8a6a2b43269774
97mams <97mams@gmail.com>
2024-12-13 18:44:52 +0300

    feat list book equal genre

--- app/book/[bookId]/page.tsx
+++ app/book/[bookId]/page.tsx
@@ -1,14 +1,12 @@
 import { prisma } from "@/src/lib/prisma"
-import { faker } from "@faker-js/faker"
+import { ListBook } from '@/src/components/listBook'
+import { Button } from "@mui/material"
 
 export default async function Page(props: {
     params: Promise<{
         bookId: number
     }>
 }) {
-
-    const fakeData = faker.date.anytime
-
     const id = Number((await props.params).bookId)
 
     const book = await prisma.books.findUnique({
@@ -16,10 +14,34 @@ export default async function Page(props: {
             id: id
         }
     })
-    console.log(book);
 
+    let genre = ""
+
+    if (book) {
+        genre = book.types
+    }
+    const books = await prisma.books.findMany({
+        where: {
+            types: genre
+        }
+    })
+
+    const bookRender = books.filter(b => b.id !== book?.id)
 
     return (
-        
+        <div className="p-8 w-full">
+            <div className="flex m-auto mb-8 gap-2 w-8/12">
+                <img className="w-1/2 object-cover" src={`/uploads/${book?.mockupImages}`} />
+                <div className="flex items-start flex-col gap-4 w-1/2">
+                    <h2>{book?.title}</h2>
+                    <p className="text-sm">{book?.resume}</p>
+                    <i>{book?.author}</i>
+                    <div>
+                        <button className="border border-border rounded-md">prendre</button>
+                    </div>
+                </div>
+            </div>
+            <ListBook data={bookRender} title="Même genre" />
+        </div>
     )
 }
