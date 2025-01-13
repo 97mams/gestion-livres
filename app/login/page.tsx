7a521af89a0f83139489649d0dc917fa7902705f
97mams <97mams@gmail.com>
2025-01-13 17:10:51 +0300

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
 
     return (
         < main className="flex items-center justify-center md:h-screen" >
-            <div className="relative flex w-full max-w-[400px] border border-border flex-col space-y-2.5 md:-mt-32">
+            <div className="relative flex w-full max-w-[400px] border border-border flex-col">
                 <LoginForm />
             </div>
             <div>{session?.user?.name}</div>
