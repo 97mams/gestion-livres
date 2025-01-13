b3f1da5bab0a2792f16f6ef71efe5e62ff3c825f
97mams <97mams@gmail.com>
2025-01-13 17:06:01 +0300

    changed members to user

--- src/components/admin/listUser.tsx
+++ src/components/admin/listUser.tsx
@@ -4,20 +4,20 @@ import { Table, TableBody, TableHeader, TableTd, TableTh, TableTr } from "@/src/
 import { useEffect, useState } from "react"
 import { Modal } from "@/src/components/modal"
 
-interface member {
+interface user {
     id: number,
-    firstName: string,
+    name: string,
     lastName: string,
     email: string,
-    contact: string
+    tel: string
 }
 
 export function ListUser() {
-    const [users, setUsers] = useState<member[]>()
+    const [users, setUsers] = useState<user[]>()
     const title: string[] = ["id", "Nom", "Prénoms", "Mail", "Tel"]
 
     useEffect(() => {
-        fetch('http://127.0.0.1:3000/api/admin/members')
+        fetch('http://127.0.0.1:3000/api/admin/users')
             .then((res) => res.json())
             .then((data) => {
                 setUsers(data.members)
@@ -36,12 +36,12 @@ export function ListUser() {
                     {users?.map(user => (
                         <TableTr key={user.id}>
                             <TableTd>{user.id}</TableTd>
-                            <TableTd>{user.firstName}</TableTd>
+                            <TableTd>{user.name}</TableTd>
                             <TableTd>{user.lastName}</TableTd>
                             <TableTd>{user.email}</TableTd>
                             <TableTd>
                                 <div className="w-full flex justify-between">
-                                    {user.contact}
+                                    {user.tel}
                                     <Modal id={user.id} />
                                 </div>
                             </TableTd>
