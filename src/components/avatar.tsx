cc7194bdfc59b28ec386c4bc3c30dfcc10d63d7e
97mams <97mams@gmail.com>
2025-01-24 10:56:00 +0300

    add event dropdown

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,8 +1,12 @@
-import { useMemo } from 'react';
+"use client"
+
+import { useMemo, useState } from 'react';
 import { createAvatar } from '@dicebear/core';
 import { lorelei } from '@dicebear/collection';
+import { Drop } from './dropProfil';
 
 export function Avatar(props: { email: string }) {
+    const [drop, setDrop] = useState(false)
     const avatar = useMemo(() => {
         return createAvatar(lorelei, {
             size: 128,
@@ -11,5 +15,19 @@ export function Avatar(props: { email: string }) {
         }).toDataUri();
     }, []);
 
-    return <img src={avatar} alt="Avatar" className="w-10 h-10 rounded-full" />;
+    const handlerDrop = () => {
+        setDrop(!drop)
+    }
+
+    return (
+        <div>
+            <img
+                src={avatar}
+                alt="Avatar"
+                className="w-10 h-10 rounded-full"
+                onClick={handlerDrop}
+            />
+            {drop ? <Drop /> : ''}
+        </div>
+    );
 }
