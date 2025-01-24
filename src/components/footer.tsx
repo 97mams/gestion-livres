f415be366e6740cb2628dbf07e6ee0eb9af9995d
97mams <97mams@gmail.com>
2025-01-24 09:03:05 +0300

    add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+export function Footer() {
+    return (
+        <div className="h-72 bg-card sticky mt-8 flex flex-col">
+            <div className="flex-1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque facere dicta quos cupiditate ad ducimus unde ipsa harum! Architecto odio obcaecati officia perspiciatis laborum iste quis sint. Natus, praesentium blanditiis.
+            </div>
+            <div className="bg-black w-full flex justify-center">
+                <p>copy right by anjaniainamamisoa@gmail.com</p>
+            </div>
+        </div>
+    )
+}
