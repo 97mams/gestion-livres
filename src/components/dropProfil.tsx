82042baade9960e2fd7065a2ddc802ba38fe61bf
97mams <97mams@gmail.com>
2025-01-24 19:10:39 +0300

    feat dropdown profil

---
+++ src/components/dropProfil.tsx
@@ -0,0 +1,11 @@
+export function Drop() {
+    return (
+        <div className="h-72 w-48 p-8 rounded-[5px] bg-white  absolute right-36 top-16">
+            <ul className="flex flex-col gap-2">
+                <li><a href="#">edit</a></li>
+                <li><a href="/emprunt">liste emprunt</a></li>
+            </ul>
+
+        </div>
+    )
+}
