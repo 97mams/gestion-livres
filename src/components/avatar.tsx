5c3c0759401a1ae7426a07ba42dc15768ac32dca
97mams <97mams@gmail.com>
2025-01-25 07:50:21 +0300

    fix dropdown

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,12 +1,13 @@
 "use client"
 
-import { useMemo, useState } from 'react';
+import { useEffect, useRef, useMemo, useState } from 'react';
 import { createAvatar } from '@dicebear/core';
 import { lorelei } from '@dicebear/collection';
-import { Drop } from './dropProfil';
+import { LogOut } from './logout';
 
 export function Avatar(props: { email: string }) {
     const [drop, setDrop] = useState(false)
+    const ref = useRef<HTMLImageElement>(null);
     const avatar = useMemo(() => {
         return createAvatar(lorelei, {
             size: 128,
@@ -14,6 +15,18 @@ export function Avatar(props: { email: string }) {
 
         }).toDataUri();
     }, []);
+    useEffect(() => {
+        const handleOutsideClick = (event: MouseEvent) => {
+
+            if (!ref.current?.contains(event.target as Node)) {
+                setDrop(false)
+            }
+
+        }
+        window.addEventListener('click', handleOutsideClick)
+
+        return () => window.removeEventListener('click', handleOutsideClick)
+    }, [ref])
 
     const handlerDrop = () => {
         setDrop(!drop)
@@ -26,8 +39,17 @@ export function Avatar(props: { email: string }) {
                 alt="Avatar"
                 className="w-10 h-10 rounded-full"
                 onClick={handlerDrop}
+                ref={ref}
             />
-            {drop ? <Drop /> : ''}
+            {drop ?
+                <div className="h-47 bg-card-foreground border border-card w-72 rounded-xl absolute right-36 top-16">
+                    <div className="flex flex-col text-card p-2 gap-2">
+                        <a className='p-2 hover:bg-card rounded-xl hover:text-foreground' href="/emprunt">dasbord</a>
+                        <a className='p-2 hover:bg-card rounded-xl hover:text-foreground' href="#">Profile</a>
+                        <LogOut classname='p-2 hover:bg-card rounded-xl hover:text-foreground' />
+                    </div>
+
+                </div> : ''}
         </div>
     );
 }
