7f43403b647fbb1ed721e546fc528d9ee204ecec
97mams <97mams@gmail.com>
2025-01-29 16:08:30 +0300

    fix type id

--- app/admin/users/update/[userId]/page.tsx
+++ app/admin/users/update/[userId]/page.tsx
@@ -2,14 +2,14 @@ import { UserForm } from "@/src/components/form"
 import { prisma } from "@/src/lib/prisma";
 
 export default async function Page(props: Promise<{
-    params: { userId: number }
+    params: { userId: string }
 }>) {
 
     const getParams = (await props).params
-    const id = Number(getParams.userId)
+    const id = getParams.userId
     console.log(id);
 
-    const users = await prisma.members.findFirst({ where: { id } })
+    const users = await prisma.user.findFirst({ where: { id } })
 
     return (
         <div className="w-full p-8">
