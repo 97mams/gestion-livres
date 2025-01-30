d5f3af8fe1245bc72534509129bf34c857fe719d
97mams <97mams@gmail.com>
2025-01-30 17:11:45 +0300

    reset ui

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,53 +1,53 @@
 import { PropsWithChildren } from "react";
 
 export function Table(props: PropsWithChildren) {
-    return (
-        <div className=" overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left rtl:text-right  text-foreground border border-border">
-                {props.children}
-            </table>
-        </div>
-    )
+  return (
+    <div className=" overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left rtl:text-right  text-foreground border border-border">
+        {props.children}
+      </table>
+    </div>
+  )
 }
 
 export function TableHeader(props: PropsWithChildren) {
-    return (
-        <thead className="text-xs bg-gray-700 text-foreground">
-            <tr>
-                {props.children}
-            </tr>
-        </thead>
-    )
+  return (
+    <thead className="text-xs bg-gray-700 text-foreground">
+      <tr>
+        {props.children}
+      </tr>
+    </thead>
+  )
 }
 
 export function TableTh(props: PropsWithChildren) {
-    return (
-        <th scope="col" className="px-6 py-3">
-            {props.children}
-        </th>
-    )
+  return (
+    <th scope="col" className="px-6 py-3">
+      {props.children}
+    </th>
+  )
 }
 
 export function TableBody(props: PropsWithChildren) {
-    return (
-        <tbody>
-            {props.children}
-        </tbody>
-    )
+  return (
+    <tbody>
+      {props.children}
+    </tbody>
+  )
 }
 
 export function TableTd(props: PropsWithChildren) {
-    return (
-        <td scope="col" className="px-6 py-4">
-            {props.children}
-        </td>
-    )
+  return (
+    <td scope="col" className="px-6 py-4">
+      {props.children}
+    </td>
+  )
 }
 
 export function TableTr(props: PropsWithChildren) {
-    return (
-        <tr className="bg-background">
-            {props.children}
-        </tr>
-    )
+  return (
+    <tr className="bg-background">
+      {props.children}
+    </tr>
+  )
 }
