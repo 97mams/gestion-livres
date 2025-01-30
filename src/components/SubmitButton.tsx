0625576cd2f74b44c53fcfddf8e5f037b8f93777
97mams <97mams@gmail.com>
2025-01-30 17:10:57 +0300

    add props classname

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,7 +1,8 @@
-export const SubmitButton = (props: { text: string, pending: boolean }) => {
+export const SubmitButton = (props: { text: string, pending: boolean, classname?: string }) => {
     return (
         <button
-            className="mt-4 border border-primary text-primary px-3 py-1 rounded-xl hover:bg-primary/20"
+            className={`mt-4 border border-primary text-primary px-3 py-1 rounded-xl hover:bg-primary/20 ${props.classname}`}
+            type="submit"
         >
             {props.pending ? "Loading..." : props.text}
         </button>
