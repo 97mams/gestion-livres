76f826a10877d6e1754a4e13bdfa7d770e57538f
97mams <97mams@gmail.com>
2025-03-06 08:43:08 +0300

    update code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 import { LoginForm } from "@/src/components/login-form"
 import { auth } from "@/src/lib/auth"
 import { redirect } from "next/navigation"
+import { User } from "@prisma/client"
 
 export default async function Page() {
     const session = await auth()
@@ -10,10 +11,9 @@ export default async function Page() {
 
     return (
         < main className="flex items-center justify-center md:h-screen" >
-            <div className="relative flex w-full max-w-[400px] border border-border flex-col">
+            <div className="relative flex  border border-border flex-col">
                 <LoginForm />
             </div>
-            <div>{session?.user?.name}</div>
         </main >
     )
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <div className="h-16 bg-black w-full flex items-center sticky bottom-0 justify-center">
+        <div className="h-16 bg-black w-full flex items-center fixed bottom-0 justify-center">
             <p>copy right by anjaniainamamisoa@gmail.com</p>
         </div>
     )
