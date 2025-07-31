1dbf1af3ff369ee74ee72ad92888235777b6cbdc
97mams <97mams@gmail.com>
2025-07-31 14:30:00 +0300

    feat: add redirect root

--- app/admin/book/page.tsx
+++ app/admin/book/page.tsx
@@ -1,6 +1,9 @@
 import { ListBook } from "@/components/admin/listBook";
+import { redirect } from "next/navigation";
 
 export default function Page() {
+  redirect("/admin/users");
+
   return (
     <div className="w-full">
       <ListBook />
