2efb7d1119e9dbfdacf0f78859ffddc6cb0970de
97mams <97mams@gmail.com>
2025-08-25 18:01:14 +0300

    fix: delete compent unuse

--- src/components/empruntCurrent.tsx
+++ src/components/empruntCurrent.tsx
@@ -1,38 +1,45 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { path } from '../lib/pathHelper';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { prisma } from '../lib/prisma';
-import useSWR from 'swr'
+import { path } from "../lib/pathHelper";
+import useSWR from "swr";
 
-const fetcher = (url: string) => fetch(url).then(resp => resp.json())
+const fetcher = (url: string) => fetch(url).then((resp) => resp.json());
 
-export function EmpruntCurrent({ userEmail }: { userEmail: string | null | undefined }) {
-    const { data, error, isLoading } = useSWR(`/api/emprunts?email=${userEmail}`, fetcher)
+export function EmpruntCurrent({
+  userEmail,
+}: {
+  userEmail: string | null | undefined;
+}) {
+  const { data, error, isLoading } = useSWR(
+    `/api/emprunts?email=${userEmail}`,
+    fetcher
+  );
 
+  if (error) {
+    throw new Error(error);
+  }
 
-    return (
-        <div className='fixed right-0 top-20'>
-            {isLoading ?
-                ''
-                :
-                <div className='min-w-50 mt-8 mx-8'>
-                    <p className='font-medium'>Votre livre</p>
-                    <ul>
-                        {data?.map((emprunt: any) => (
-                            <li key={(emprunt.id)}>
-                                <a
-                                    href={`/book/${path(emprunt.book.types)}/${emprunt.book.id}`}
-                                    className='hover:underline w-full inline-block no-underline transition-colors hower:text-foreground text-muted-foreground overflow'
-                                >
-                                    {emprunt.book.title}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            }
+  return (
+    <div className="fixed right-0 top-20">
+      {isLoading ? (
+        ""
+      ) : (
+        <div className="min-w-50 mt-8 mx-8">
+          <p className="font-medium">Votre livre</p>
+          <ul>
+            {data?.map((emprunt: any) => (
+              <li key={emprunt.id}>
+                <a
+                  href={`/book/${path(emprunt.book.types)}/${emprunt.book.id}`}
+                  className="hover:underline w-full inline-block no-underline transition-colors hower:text-foreground text-muted-foreground overflow"
+                >
+                  {emprunt.book.title}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+      )}
+    </div>
+  );
 }
