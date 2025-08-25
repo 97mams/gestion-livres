04cd0e98a089570d0d52de0e35d9baea036b36a2
97mams <97mams@gmail.com>
2025-08-25 16:00:42 +0300

    fix: format code

--- src/components/bookCard.tsx
+++ src/components/bookCard.tsx
@@ -1,21 +1,33 @@
-export function BookCard(props: {
-    title?: string,
-    image?: string,
-    description?: string,
-    author: string,
-    date?: string,
-    bkey?: number
-}) {
-    return (
-        <div className="max-w-60 bg-card shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl" key={props.bkey}>
-            <img src={`/uploads/${props.image}`}
-                alt="Product" className="h-40 w-72 object-cover rounded-t-xl" />
-            <div className="px-4 py-3">
-                <span className="text-gray-400 mr-3 uppercase text-xs">{props.author}</span>
-                <p className="text-lg font-bold text-forground truncate block capitalize">{props.title}</p>
+import Image from "next/image";
 
-                <p className="text-sm text-primary">Aimés(100)</p>
-            </div>
-        </div>
-    )
+export function BookCard(props: {
+  title?: string;
+  image?: string;
+  description?: string;
+  author: string;
+  date?: string;
+  bkey?: number;
+}) {
+  return (
+    <div
+      className="max-w-60 bg-card shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+      key={props.bkey}
+    >
+      <Image
+        src={`/uploads/${props.image}`}
+        alt="Product"
+        className="h-40 w-72 object-cover rounded-t-xl"
+      />
+      <div className="px-4 py-3">
+        <span className="text-gray-400 mr-3 uppercase text-xs">
+          {props.author}
+        </span>
+        <p className="text-lg font-bold text-forground truncate block capitalize">
+          {props.title}
+        </p>
+
+        <p className="text-sm text-primary">Aimés(100)</p>
+      </div>
+    </div>
+  );
 }
