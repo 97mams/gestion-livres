aba12fbd97040e3820109547bfe61106617ce38d
97mams <97mams@gmail.com>
2025-08-25 15:59:49 +0300

    fix: add key for bookCard

--- app/book/lists/page.tsx
+++ app/book/lists/page.tsx
@@ -2,44 +2,40 @@ import { prisma } from "@/src/lib/prisma";
 import { BookCard } from "@/src/components/bookCard";
 
 export default async function Page() {
-    const allBook = await prisma.books.findMany()
-    const recentBook = await prisma.books.findMany({
-        orderBy: { createdAt: "desc" },
-        take: 4,
-
-    })
-    return (
-        <div className="w-full p-4">
-            <div className="w-full">
-                <h1 className="font-bold text-foreground text-2xl">Nouvelle livres</h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
-                    {
-                        recentBook.map((book) => (
-                            <BookCard
-                                title={book.title}
-                                image={book.mockupImages}
-                                author={book.author}
-                                key={book.id}
-                            />
-                        ))
-                    }
-                </div>
-            </div>
-            <div className="w-full">
-                <h1 className="font-bold text-foreground text-2xl">Livres</h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
-                    {
-                        allBook.map((book) => (
-                            <BookCard
-                                title={book.title}
-                                image={book.mockupImages}
-                                author={book.author}
-                                bkey={book.id}
-                            />
-                        ))
-                    }
-                </div>
-            </div>
+  const allBook = await prisma.books.findMany();
+  const recentBook = await prisma.books.findMany({
+    orderBy: { createdAt: "desc" },
+    take: 4,
+  });
+  return (
+    <div className="w-full p-4">
+      <div className="w-full">
+        <h1 className="font-bold text-foreground text-2xl">Nouvelle livres</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+          {recentBook.map((book) => (
+            <BookCard
+              title={book.title}
+              image={book.mockupImages}
+              author={book.author}
+              key={book.id}
+            />
+          ))}
         </div>
-    )
+      </div>
+      <div className="w-full">
+        <h1 className="font-bold text-foreground text-2xl">Livres</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+          {allBook.map((book) => (
+            <BookCard
+              key={book.id}
+              title={book.title}
+              image={book.mockupImages}
+              author={book.author}
+              bkey={book.id}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
