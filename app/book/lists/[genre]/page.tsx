0858d07ecfcfa01427491825f6462d3f25eb043d
97mams <97mams@gmail.com>
2025-08-25 18:17:11 +0300

    fix: add ame fuction

--- app/book/lists/[genre]/page.tsx
+++ app/book/lists/[genre]/page.tsx
@@ -1,30 +1,28 @@
-import { prisma } from "@/src/lib/prisma"
-import { ListBook } from "@/src/components/listBook"
-import { BackButton } from "@/src/components/backButton"
+import { prisma } from "@/src/lib/prisma";
+import { ListBook } from "@/src/components/listBook";
+import { BackButton } from "@/src/components/backButton";
 
-export default async function (props: {
-    params:
-    Promise<{
-        genre: string
-    }>
+export default async function Page(props: {
+  params: Promise<{
+    genre: string;
+  }>;
 }) {
+  const genreParams = (await props.params).genre;
+  let genre = genreParams;
+  if (genreParams.includes("_")) {
+    const genreArray = genreParams.split("_");
+    genre = genreArray[0] + " " + genreArray[1];
+  }
+  const bookByGenre = await prisma.books.findMany({
+    where: {
+      types: genre,
+    },
+  });
 
-    const genreParams = (await props.params).genre
-    let genre = genreParams
-    if (genreParams.includes("_")) {
-        const genreArray = genreParams.split('_')
-        genre = genreArray[0] + ' ' + genreArray[1]
-    }
-    const bookByGenre = await prisma.books.findMany({
-        where: {
-            types: genre,
-        },
-    })
-
-    return (
-        <div>
-            <BackButton />
-            <ListBook data={bookByGenre} title={genre} />
-        </div>
-    )
-} 
+  return (
+    <div>
+      <BackButton />
+      <ListBook data={bookByGenre} title={genre} />
+    </div>
+  );
+}
