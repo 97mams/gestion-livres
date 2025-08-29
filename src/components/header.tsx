a9daa6b39b16e6a3dfd0a16d9b0af660bcf00a7d
97mams <97mams@gmail.com>
2025-08-29 16:14:01 +0300

    [fix] change logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { auth } from "../lib/auth";
 import { Avatar } from "./avatar";
+import { GithubIcon } from "./GithubIcon";
 
 export async function Header() {
   const session = await auth();
@@ -11,7 +12,9 @@ export async function Header() {
     <nav className="sticky top-0 z-40 bg-accent text-foreground">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <span className="text-2xl font-semibold">Boky</span>
+          <span className="text-2xl font-semibold">
+            <Image src="/icon.png" alt="Logo" width={40} height={40} />
+          </span>
           <div className="flex space-x-4 items-baseline">
             {session ? (
               <div className="flex gap-4 items-center">
@@ -22,13 +25,7 @@ export async function Header() {
               ""
             )}
             <Link href="/" className="flex items-center">
-              <Image
-                src="/logo.png"
-                alt="githubIcon"
-                width={40}
-                height={40}
-                className="rounded-full"
-              />
+              <GithubIcon size={30} />
             </Link>
           </div>
         </div>
