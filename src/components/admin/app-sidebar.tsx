1c7424037853bb8f995fb2636636d450575975a2
97mams <97mams@gmail.com>
2025-09-01 17:30:15 +0300

    feat: add items

---
+++ src/components/admin/app-sidebar.tsx
@@ -0,0 +1,64 @@
+import { Book, Mail, User } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+// Menu items.
+const items = [
+  {
+    title: "Utilisateurs",
+    url: "/admin/users",
+    icon: User,
+  },
+  {
+    title: "Livres",
+    url: "/admin/books",
+    icon: Book,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url} className="hover:bg-muted">
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <Link
+          target=""
+          href={"anjaniainamamisoa@gmail.com"}
+          className="flex gap-2"
+        >
+          <Mail size={25} />
+          97mams@gmail.com
+        </Link>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
